5be4ec2a4c665ccbbc5f8166ac19e2668852b793
Qiao Su <tracy.xin.li@gmail.com>
2012-11-07 23:59:28 +0800

    +jsonpi

--- javascripts/main.js
+++ javascripts/main.js
@@ -3,13 +3,43 @@ $(document).ready(function() {
 		$.ajax({
 	    	type: 'POST',
 	      	url: /*/\?dev/.test(window.location) ? */'http://localhost:4567', /*: 'http://bootstrap.herokuapp.com'*/
-	      	dataType: 'jsonp',
-	      	jsonp: 'callback',
-	      	async: false,
+	      	dataType: 'jsonpi',
 	        params: {
-	          	source: 'klass**event'
+	          	source: 'klass'
 	        }
 	      })
 		e.preventDefault();
 	});
+
+	// Modified from the original jsonpi https://github.com/benvinegar/jquery-jsonpi
+$.ajaxTransport('jsonpi', function(opts, originalOptions, jqXHR) {
+  var url = opts.url;
+
+  return {
+    send: function(_, completeCallback) {
+      var name = 'jQuery_iframe_' + jQuery.now()
+        , iframe, form
+
+      iframe = $('<iframe>')
+        .attr('name', name)
+        .appendTo('head')
+
+      form = $('<form>')
+        .attr('method', opts.type) // GET or POST
+        .attr('action', url)
+        .attr('target', name)
+
+      $.each(opts.params, function(k, v) {
+
+        $('<input>')
+          .attr('type', 'hidden')
+          .attr('name', k)
+          .attr('value', typeof v == 'string' ? v : JSON.stringify(v))
+          .appendTo(form)
+      })
+
+      form.appendTo('body').submit()
+    }
+  }
+})
 });
