136284c6442ae158bb4c030f96f14bd473486e4a
Qiao Su <tracy.xin.li@gmail.com>
2012-11-07 23:36:50 +0800

    +jsonp

--- javascripts/main.js
+++ javascripts/main.js
@@ -1,6 +1,13 @@
 $(document).ready(function() {
 	$('#J_ns_download').on('click', function(e){
-		console.log('ready');
-		e.proventDefault();
+		$.ajax({
+	    	type: 'POST',
+	      	url: /*/\?dev/.test(window.location) ? */'http://localhost:3000', /*: 'http://bootstrap.herokuapp.com'*/
+	      	dataType: 'jsonpi',
+	        params: {
+	          	source: ['klass','event']
+	        }
+	      })
+		e.preventDefault();
 	});
 });
