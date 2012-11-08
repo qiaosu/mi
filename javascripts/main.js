5a9ab20ac8cd0724d953842ad1478413b1cabbb5
Qiaosu <tracy.xin.li@gmail.com>
2012-11-08 10:38:38 +0800

    +namespace support

--- javascripts/main.js
+++ javascripts/main.js
@@ -1,45 +1,66 @@
 $(document).ready(function() {
 	$('#J_ns_download').on('click', function(e){
+		//check submit data
+		var ns, source;
+		ns = $('input[name="namespace"]').val().trim();
+		if (ns === '') {
+			view.showError('namespace is required.');
+			return false;
+		}
+		if (!ns.match(/^[a-zA-Z_]+$/)) {
+			view.showError('please use "a-z, A-Z, or _".');
+			return false;
+		}
+
 		$.ajax({
 	    	type: 'POST',
 	      	url: /*/\?dev/.test(window.location) ? */'http://localhost:4567', /*: 'http://bootstrap.herokuapp.com'*/
 	      	dataType: 'jsonpi',
 	        params: {
-	          	source: 'klass'
+	          	source: 'klass',
+	          	namespace: ns
 	        }
 	      })
 		e.preventDefault();
 	});
 
 	// Modified from the original jsonpi https://github.com/benvinegar/jquery-jsonpi
-$.ajaxTransport('jsonpi', function(opts, originalOptions, jqXHR) {
-  var url = opts.url;
+	$.ajaxTransport('jsonpi', function(opts, originalOptions, jqXHR) {
+	  	var url = opts.url;
 
-  return {
-    send: function(_, completeCallback) {
-      var name = 'jQuery_iframe_' + jQuery.now()
-        , iframe, form
+	  	return {
+	    	send: function(_, completeCallback) {
+	      		var name = 'jQuery_iframe_' + jQuery.now()
+	        	, iframe, form
 
-      iframe = $('<iframe>')
-        .attr('name', name)
-        .appendTo('head')
+	      		iframe = $('<iframe>')
+	        	.attr('name', name)
+	        	.appendTo('head')
 
-      form = $('<form>')
-        .attr('method', opts.type) // GET or POST
-        .attr('action', url)
-        .attr('target', name)
+	      		form = $('<form>')
+	        	.attr('method', opts.type) // GET or POST
+	        	.attr('action', url)
+	        	.attr('target', name)
 
-      $.each(opts.params, function(k, v) {
+	      		$.each(opts.params, function(k, v) {
 
-        $('<input>')
-          .attr('type', 'hidden')
-          .attr('name', k)
-          .attr('value', typeof v == 'string' ? v : JSON.stringify(v))
-          .appendTo(form)
-      })
+	        		$('<input>')
+	          		.attr('type', 'hidden')
+	          		.attr('name', k)
+	          		.attr('value', typeof v == 'string' ? v : JSON.stringify(v))
+	          		.appendTo(form)
+	      		})
 
-      form.appendTo('body').submit()
-    }
-  }
-})
+	      		form.appendTo('body').submit()
+	    	}
+	  	}
+	});
+
+	var view = {
+		showError: function(msg){
+			var tgt = $('input[name="namespace"]').next('.item-explain');
+			tgt.html(msg);
+			tgt.addClass('error');
+		}
+	}
 });
